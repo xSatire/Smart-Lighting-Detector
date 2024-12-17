9aed27a3a9af09aea48390f1ea43fe99687d9217
xSatire <newyplayz@gmail.com>
2024-12-17 20:36:37 +0800

    reverted the changes

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import SignInButton from "@/components/auth/signInButton";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <SignInButton></SignInButton>
+    </div>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -11,8 +11,7 @@ export const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const { nextUrl } = req;
-  // const isLoggedIn = !!req.auth;
-  const isLoggedIn = true; //
+  const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
